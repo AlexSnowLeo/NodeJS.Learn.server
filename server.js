d1ca3404c886ffb1f929ffa8172f466308fc8283
SnowLeo <alex.v.timofeev@gmail.com>
2015-08-14 17:21:44 +0500

    Урок 15: Эхо-сервер на Node.JS

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// http://127.0.0.1/echo?message=Hello -> Hello
+
 var http = require('http');
+var url = require('url');
 
 var server = new http.Server();
-// http.Server -> net.Server -> EventEmitter
-
-server.listen(1337, '127.0.0.1');
-
-var counter = 0;
-
-var emit = server.emit;
-server.emit = function(event) {
-    console.log(event);
-    emit.apply(server, arguments);
-};
 
 server.on('request', function (req, res) {
-    res.end("Привет, мир! " + ++counter);
+    console.log(req.method, req.url);
+
+    var urlParsed = url.parse(req.url, true);
+    console.log(urlParsed);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        //res.writeHead(200, "OK", {'Cache-control': 'no-cache'}); // немедленный вывод заголовка браузеру, не ожидая ближайшего вывода
+        res.setHeader('Cache-control', 'no-cache'); // нормальный вывод заголовка
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
 });
+
+server.listen(1337, '127.0.0.1');
